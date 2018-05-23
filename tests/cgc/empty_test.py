4182531dad99c8c588d28cc05b8672a7bc31c5da
JeanBilheux <bilheuxjm@ornl.gov>
2018-05-23 15:58:21 -0400

    loading list of file names and unit test

--- tests/cgc/empty_test.py
+++ tests/cgc/empty_test.py
@@ -1,5 +1,6 @@
 import unittest
-import numpy as np
+import glob
+import os
 
 from cgc.geometry_correction import GeometryCorrection
 
@@ -7,15 +8,23 @@ from cgc.geometry_correction import GeometryCorrection
 class TestNothing(unittest.TestCase):
 
     def setUp(self):
-        pass
+        _file_path = os.path.dirname(__file__)
+        self.data_path = os.path.abspath(os.path.join(_file_path, "../../notebooks/data/"))
 
     def test_list_files_has_correct_format(self):
         '''assert the list of files is a non empty and existing list of files'''
 
-        # string not allowed
+        # string not allowed during initialization
         list_files = ''
         self.assertRaises(TypeError, GeometryCorrection, list_files=list_files)
 
         # name of files should exist
         list_files = ['i_do_not_exist.tiff']
         self.assertRaises(ValueError, GeometryCorrection, list_files=list_files)
+
+        # files exist
+        list_files = glob.glob(self.data_path + '/*.fits')
+        o_cgc = GeometryCorrection(list_files=list_files)
+        list_files_returned = o_cgc.list_files
+        self.assertTrue(list_files == list_files_returned)
+
