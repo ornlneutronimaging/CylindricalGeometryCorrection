ccfcbaa016609e3b0946b5b274ac042eaca2af5a
JeanBilheux <bilheuxjm@ornl.gov>
2018-05-23 15:00:31 -0400

    Added first empty test

---
+++ tests/cgc/empty_test.py
@@ -0,0 +1,12 @@
+import unittest
+import numpy as np
+
+import cgc
+
+class TestNothing(unittest.TestCase):
+
+    def setUp(self):
+        pass
+
+    def test_nothing(self):
+        self.assertTrue(True)
