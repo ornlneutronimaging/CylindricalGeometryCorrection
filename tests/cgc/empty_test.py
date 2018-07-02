7bd70a257c9fc9d44e60e3c44067ebf0288a1ba2
JeanBilheux <bilheuxjm@ornl.gov>
2018-07-02 12:04:57 -0400

    Implemented unit test for inhomogenous samples

--- tests/cgc/empty_test.py
+++ tests/cgc/empty_test.py
@@ -136,7 +136,7 @@ class testHomogeneousCorrection(unittest.TestCase):
         self.data_path = os.path.abspath(os.path.join(_file_path, "../../notebooks/data/"))
 
     def test_calculate_sample_thickness(self):
-        """assert calculation of thickness is correct for homo and inhomogeneous"""
+        """assert calculation of thickness is correct for homogenous sample"""
 
         list_fits = glob.glob(self.data_path + '/fits/homogeneous*.fits')
         o_cgc = GeometryCorrection(list_files=list_fits)
@@ -145,10 +145,6 @@ class testHomogeneousCorrection(unittest.TestCase):
         # homogeneous sample
         o_cgc.define_parameters(pixel_center=100, outer_radius=50)
         self.assertEqual(o_cgc.get_sample_thickness_at_center(), 100)
-
-        # inhomogeneous sample
-        o_cgc.define_parameters(pixel_center=100, outer_radius=50, inner_radius=70)
-        self.assertEqual(o_cgc.get_sample_thickness_at_center(), 40)
 
     def test_calculate_pixel_intensity(self):
         """assert calculation of pixel_intensity works"""
@@ -209,81 +205,81 @@ class testHomogeneousCorrection(unittest.TestCase):
         for _returned, _expected in zip(row_10_returned, row_10_expected):
             self.assertAlmostEqual(_returned, _expected, delta=0.1)
 
+
 class testInhomogeneousCorrection(unittest.TestCase):
 
     def setUp(self):
         _file_path = os.path.dirname(__file__)
         self.data_path = os.path.abspath(os.path.join(_file_path, "../../notebooks/data/"))
 
-    # def test_calculate_sample_thickness(self):
-    #     """assert calculation of thickness is correct for homo and inhomogeneous"""
-    #
-    #     list_fits = glob.glob(self.data_path + '/fits/inhomogeneous*.fits')
-    #     o_cgc = GeometryCorrection(list_files=list_fits)
-    #     o_cgc.load_files()
-    #
-    #     # homogeneous sample
-    #     o_cgc.define_parameters(pixel_center=100, outer_radius=50)
-    #     self.assertEqual(o_cgc.get_sample_thickness_at_center(), 100)
-    #
-    #     # inhomogeneous sample
-    #     o_cgc.define_parameters(pixel_center=100, outer_radius=50, inner_radius=70)
-    #     self.assertEqual(o_cgc.get_sample_thickness_at_center(), 40)
-    #
-    # def test_calculate_pixel_intensity(self):
-    #     """assert calculation of pixel_intensity works"""
-    #     list_fits = glob.glob(self.data_path + '/tiff/homogeneous*.tif')
-    #     o_cgc = GeometryCorrection(list_files=list_fits)
-    #     o_cgc.load_files()
-    #     o_cgc.define_parameters(pixel_center=256, outer_radius=200)
-    #     _image_0 = o_cgc.list_data[0]
-    #     _slice_50 = _image_0[50, :]
-    #     self.assertAlmostEqual(2.00,
-    #                            o_cgc.calculate_pixel_intensity(slice=_slice_50),
-    #                            delta=ERR_OFFSET)
+    def test_calculate_sample_thickness(self):
+        """assert calculation of thickness is correct for inhomogeneous"""
 
-    # def test_isolate_cylinder_from_image(self):
-    #     """assert isolation of cylinder works for homogeneous and inhomogeneous"""
-    #     list_tiff = glob.glob(self.data_path + '/tiff/homogeneous*.tif')
-    #     o_cgc = GeometryCorrection(list_files=list_tiff)
-    #     o_cgc.load_files()
-    #     pixel_center = 256
-    #     radius = 200
-    #     o_cgc.define_parameters(pixel_center=pixel_center, outer_radius=radius)
-    #     _isolated_cylinder_calculated = o_cgc.isolate_cylinder_from_image(index=0)
-    #
-    #     # what we expect
-    #     image_0 = o_cgc.list_data[0]
-    #     _isolated_cylinder_expected = image_0[:, pixel_center-radius:pixel_center+radius+1]
-    #     self.assertTrue((_isolated_cylinder_calculated == _isolated_cylinder_expected).all())
-    #
-    # def test_correction_intensity_2(self):
-    #     """assert the correction works"""
-    #     tiff_image = glob.glob(self.data_path + '/tiff/homogeneous_image_px_intensity_2.tif')
-    #     o_cgc = GeometryCorrection(list_files=tiff_image)
-    #     o_cgc.load_files()
-    #     pixel_center = 256
-    #     radius = 200
-    #     o_cgc.define_parameters(pixel_center=pixel_center, outer_radius=radius)
-    #     o_cgc.correct()
-    #     first_image_corrected = o_cgc.list_data_corrected[0]
-    #     row_10_returned = first_image_corrected[10, :]
-    #     row_10_expected = np.ones(400)*2
-    #     for _returned, _expected in zip(row_10_returned, row_10_expected):
-    #         self.assertAlmostEqual(_returned, _expected, delta=0.1)
-    #
-    # def test_correction_intensity_4(self):
-    #     """assert the correction works"""
-    #     tiff_image = glob.glob(self.data_path + '/tiff/homogeneous_image_px_intensity_4.tif')
-    #     o_cgc = GeometryCorrection(list_files=tiff_image)
-    #     o_cgc.load_files()
-    #     pixel_center = 256
-    #     radius = 200
-    #     o_cgc.define_parameters(pixel_center=pixel_center, outer_radius=radius)
-    #     o_cgc.correct()
-    #     first_image_corrected = o_cgc.list_data_corrected[0]
-    #     row_10_returned = first_image_corrected[10, :]
-    #     row_10_expected = np.ones(400)*4
-    #     for _returned, _expected in zip(row_10_returned, row_10_expected):
-    #         self.assertAlmostEqual(_returned, _expected, delta=0.1)
+        list_fits = glob.glob(self.data_path + '/fits/inhomogeneous*.fits')
+        o_cgc = GeometryCorrection(list_files=list_fits)
+        o_cgc.load_files()
+
+        # inhomogeneous sample
+        o_cgc.define_parameters(pixel_center=100, outer_radius=50, inner_radius=70)
+        self.assertEqual(o_cgc.get_sample_thickness_at_center(), 40)
+
+    def test_calculate_pixel_intensity(self):
+        """assert calculation of pixel_intensity works"""
+        list_fits = glob.glob(self.data_path + '/tiff/inhomogeneous*.tif')
+        o_cgc = GeometryCorrection(list_files=list_fits)
+        o_cgc.load_files()
+        o_cgc.define_parameters(pixel_center=256, inner_radius=150, outer_radius=200)
+        _image_0 = o_cgc.list_data[0]
+        _slice_50 = _image_0[50, :]
+        self.assertAlmostEqual(2.00,
+                               o_cgc.calculate_pixel_intensity(slice=_slice_50),
+                               delta=ERR_OFFSET)
+
+    def test_isolate_cylinder_from_image(self):
+        """assert isolation of cylinder works for inhomogeneous"""
+        list_tiff = glob.glob(self.data_path + '/tiff/inhomogeneous*.tif')
+        o_cgc = GeometryCorrection(list_files=list_tiff)
+        o_cgc.load_files()
+        pixel_center = 256
+        inner_radius = 150
+        outer_radius = 200
+        o_cgc.define_parameters(pixel_center=pixel_center, outer_radius=outer_radius, inner_radius=inner_radius)
+        _isolated_cylinder_calculated = o_cgc.isolate_cylinder_from_image(index=0)
+
+        # what we expect
+        image_0 = o_cgc.list_data[0]
+        _isolated_cylinder_expected = image_0[:, pixel_center-outer_radius:pixel_center+outer_radius+1]
+        self.assertTrue((_isolated_cylinder_calculated == _isolated_cylinder_expected).all())
+
+    def test_correction_intensity_2(self):
+        """assert the correction works for inhomogenous sample of intensity 2"""
+        tiff_image = glob.glob(self.data_path + '/tiff/inhomogeneous_image_px_intensity_2.tif')
+        o_cgc = GeometryCorrection(list_files=tiff_image)
+        o_cgc.load_files()
+        pixel_center = 256
+        outer_radius = 200
+        inner_radius = 150
+        o_cgc.define_parameters(pixel_center=pixel_center, outer_radius=outer_radius, inner_radius=inner_radius)
+        o_cgc.correct()
+        first_image_corrected = o_cgc.list_data_corrected[0]
+        row_10_returned = first_image_corrected[10, :]
+        row_10_expected = np.ones(400)*2
+        for _returned, _expected in zip(row_10_returned, row_10_expected):
+            self.assertAlmostEqual(_returned, _expected, delta=0.1)
+
+    def test_correction_intensity_4(self):
+        """assert the correction works for inhomogenous sample of intensity 4"""
+        tiff_image = glob.glob(self.data_path + '/tiff/inhomogeneous_image_px_intensity_4.tif')
+        o_cgc = GeometryCorrection(list_files=tiff_image)
+        o_cgc.load_files()
+        pixel_center = 256
+        outer_radius = 200
+        inner_radius = 150
+        o_cgc.define_parameters(pixel_center=pixel_center, outer_radius=outer_radius, inner_radius=inner_radius)
+        o_cgc.correct()
+        first_image_corrected = o_cgc.list_data_corrected[0]
+        row_10_returned = first_image_corrected[10, :]
+        row_10_expected = np.ones(400)*4
+        for _returned, _expected in zip(row_10_returned, row_10_expected):
+            self.assertAlmostEqual(_returned, _expected, delta=0.1)
 
