98317192cd4cb4a61c455a603033da84acf4af6d
JeanBilheux <bilheuxjm@ornl.gov>
2021-06-08 13:09:44 -0400

    Fixed evaluate_homogenous_formula_error notebook. this refs #1

--- notebooks/geometry_correction.py
+++ notebooks/geometry_correction.py
@@ -2,12 +2,13 @@ import numpy as np
 
 
 def homogeneous_correction_factor(x=0, radius=-1):
-    if np.abs(x)>radius:
+    if np.abs(x) > radius:
         return 0
     rp = 2*radius*np.sin(np.arccos(x/radius))
     if x == 0:
         return 1
     return (2*radius)/rp
+
 
 def inhomogeneous_correction(x=-1, inner_circle_r=1, outer_circle_r=1):
     r = np.abs(x)
@@ -20,7 +21,8 @@ def inhomogeneous_correction(x=-1, inner_circle_r=1, outer_circle_r=1):
         rp2 = 2*outer_circle_r*np.sin(np.arccos(x/outer_circle_r))
         rp = rp2 - rp1
         return rp
-    
+
+
 def factor_inho(x=0, inner_circle_r=1, outer_circle_r=1):
     _value = inhomogeneous_correction(x=x, 
                                       inner_circle_r=inner_circle_r, 
@@ -29,4 +31,4 @@ def factor_inho(x=0, inner_circle_r=1, outer_circle_r=1):
         return 1
     if _value == 0:
         _value = np.NaN
-    return (2*(outer_circle_r-inner_circle_r)/_value)
+    return 2*(outer_circle_r-inner_circle_r)/_value
