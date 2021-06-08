4cb4f995cbf4078a46f39a9c05c5f882b0e60a1e
JeanBilheux <bilheuxjm@ornl.gov>
2021-06-08 11:15:07 -0400

    started to reformat notebooks to make them work. working on create_fake_cylindrical_2d_data. this refs #1

--- notebooks/file_handler.py
+++ notebooks/file_handler.py
@@ -1,31 +1,41 @@
-import pyfits
+from astropy.io import fits
 from PIL import Image
 import os
 import numpy as np
 import matplotlib.image as mpimg
 
 
-def make_fits(data=[], filename=''):
-    if os.path.exists(filename):
-        os.remove(filename)
-    hdu = pyfits.PrimaryHDU(data)
-    hdulist = pyfits.HDUList([hdu])
-    hdulist.writeto(filename)
-    hdulist.close()
-    
+# def make_fits(data=[], filename=''):
+#     if os.path.exists(filename):
+#         os.remove(filename)
+#     hdu = pyfits.PrimaryHDU(data)
+#     hdulist = pyfits.HDUList([hdu])
+#     hdulist.writeto(filename)
+#     hdulist.close()
+
+
 def make_tiff(data=[], filename=''):
     if os.path.exists(filename):
         os.remove(filename)
     _image = Image.fromarray(data)
     _image.save(filename)
-    
-def load_fits(filename=''):
-    hdu_list = pyfits.open(filename)
-    hdu = hdu_list[0]
-    _image = hdu.data
-    _image = np.asarray(_image)
-    hdu_list.close()
-    return _image
+
+
+def load_fits(filename=""):
+    '''load fits image
+
+    Parameters
+    ----------
+       full file name of fits image
+    '''
+    try:
+        tmp = fits.open(filename, ignore_missing_end=True)[0].data
+        if len(tmp.shape) == 3:
+            tmp = tmp.reshape(tmp.shape[1:])
+        return tmp
+    except OSError:
+        raise OSError("Unable to read the FITS file provided!")
+
 
 def load_tiff(filename=''):
     _image = mpimg.imread(filename)
