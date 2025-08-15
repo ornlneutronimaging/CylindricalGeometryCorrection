0934dbc72e97de60c44168ea6ebb1c1779552896
Chen Zhang <chenzhang8722@gmail.com>
2025-08-15 14:56:34 -0400

    remove pointless version test

--- tests/test_version.py
+++
@@ -1,8 +0,0 @@
-from neutron_geomcorr import __version__
-
-
-def test_version():
-    assert __version__ == "unknown" or "dev" in __version__ or "0.1.0" in __version__ or "1.0.0" in __version__
-    #      ^ conda env will default to "unknown" if not set
-    #                                  ^ pixi will default to the default tag in pyproject.toml
-    #                                    if not set, it will default to "0.1.0" + "devxxx" on local dev builds
